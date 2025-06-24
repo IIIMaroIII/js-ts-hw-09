43af69bbba852595e0129a54b04dbbd230297609
Rod <rod@rods-imac.tail98af09.ts.net>
2025-06-24 13:37:18 +0300

    finished feature/form task

--- src/js/form.js
+++ src/js/form.js
@@ -1,10 +1,31 @@
 const Form = {
+  DATA_KEY: 'form_data_key',
   formData: {
     email: '',
     message: '',
   },
   init() {
+    this.initialValues();
     this.hangListener(this.formEl, 'submit', this.onFormSubmit.bind(this));
+  },
+  savetoLocalStorage(data, key) {
+    localStorage.setItem(key, JSON.stringify(data));
+  },
+  loadFromLocalStorage(key) {
+    try {
+      const parsedData = localStorage.getItem(key);
+      if (!parsedData) return;
+      return JSON.parse(parsedData);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  initialValues() {
+    const parsedData = this.loadFromLocalStorage(this.DATA_KEY);
+    if (typeof parsedData !== 'object' || !this.formEl) return;
+
+    this.formEl.elements.email.value = parsedData.email;
+    this.formEl.elements.message.value = parsedData.message;
   },
   hangListener(el = document, evt = 'click', cb = () => {}) {
     el.addEventListener(evt, cb);
@@ -12,19 +33,11 @@ const Form = {
   onFormSubmit(e) {
     e.preventDefault();
     const { elements } = e.currentTarget;
-
-    if (this.validate(elements?.email?.value)) {
-      alert('Must not be empty');
-      return;
-    }
     this.email = elements.email.value;
-    if (this.validate(elements?.message?.value)) {
-      alert('Must not be empty');
-      return;
-    }
     this.message = elements.message.value;
 
     console.log(this.data);
+    this.savetoLocalStorage(this.data, this.DATA_KEY);
     e.currentTarget.reset();
   },
   get formEl() {
@@ -44,9 +57,6 @@ const Form = {
   },
   get data() {
     return { ...this.formData };
-  },
-  validate(v) {
-    return v.trim() === '';
   },
 };
 
